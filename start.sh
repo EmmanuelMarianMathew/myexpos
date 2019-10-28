e57e136ce42428f64c99a6148015e075affc67c8
Shreyansh-sinha <shreyanshsinha2@gmail.com>
2019-10-28 22:57:47 +0530

    first commit after so long...srry for such a big commit shtreya

---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+curr=$(pwd)
+cd $HOME/myexpos/spl/spl_progs/
+file=$(ls *.spl)
+cd ..
+for i in $file
+do
+	./spl spl_progs/$i
+	echo $i
+done
+echo "Compiled spl programs."
+cd ../expl/expl_progs/
+file=$(ls *.expl)
+cd ..
+for i in $file
+do
+	./expl expl_progs/$i
+	echo $i
+done
+echo "Compiled expl programs"
+cd ..
+cd xfs-interface/
+./xfs-interface fdisk
+./xfs-interface run run_C.txt
+cd $curr
+echo "All files loaded."
